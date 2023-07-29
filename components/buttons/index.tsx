384481522201d2f457d06a3f224423a2627a389e
MrezaVasebi <reza.vaseb@gmail.com>
2023-07-29 21:02:37 +0330

    added: threeTabButton was added

--- components/buttons/index.tsx
+++ components/buttons/index.tsx
@@ -3,5 +3,6 @@ import AppLoadingButton from "./AppLoadingButton";
 import RadioButton from "./RadioButton";
 import CheckBoxButton from "./CheckBoxButton";
 import TabButton from "./TabButton";
+import ThreeTabButton from "./ThreeTabButton";
 
-export { TabButton, CheckBoxButton, RadioButton, AppLoadingButton, AppButton };
+export { ThreeTabButton, TabButton, CheckBoxButton, RadioButton, AppLoadingButton, AppButton };
