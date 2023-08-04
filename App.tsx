0a20a84788882d716e369d92b886c9f216312857
MrezaVasebi <reza.vaseb@gmail.com>
2023-08-04 18:17:45 +0330

    added: inputWithClearButton was added

--- App.tsx
+++ App.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { RootScreen } from "./components";
 import { StyleSheet } from "react-native";
 import { appMargin } from "./utils";
+import InputWithClearButton from "./components/inputs/InputWithClearButton";
 
 const App = () => {
-   return <RootScreen rootStyle={styles.rootStyle}></RootScreen>;
+   return (
+      <RootScreen rootStyle={styles.rootStyle}>
+         <InputWithClearButton showLabel={false} onPressClear={() => {}} label="Enter Name" onChangeText={() => {}} placeholder="Name" value="" />
+      </RootScreen>
+   );
 };
 
 export default App;
---
+++ components/inputs/InputWithClearButton.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import React from "react";
+import SimpleInput from "./SimpleInput";
+import { handleIcons } from "../../modules";
+import { appColors, appRadius } from "../../utils";
+import ShowLabel from "./ShowLabel";
+
+interface IInputWithClearButton {
+   label: string;
+   root?: object;
+   value: string;
+   placeholder: string;
+   showLabel?: boolean;
+   onPressClear: () => void;
+   onChangeText: (value: string) => void;
+}
+
+const InputWithClearButton = (props: IInputWithClearButton) => {
+   let { value, placeholder, onChangeText, label, showLabel, root, onPressClear } = props;
+
+   return (
+      <View style={{ ...styles.root, ...root }}>
+         <ShowLabel label={label} isLabelShow={showLabel} />
+
+         <View style={{ ...styles.inputContainer }}>
+            <TouchableOpacity activeOpacity={0.5} onPress={onPressClear} style={styles.clearStyle}>
+               {handleIcons("close", 20, appColors.black)}
+            </TouchableOpacity>
+
+            <SimpleInput inputStyle={{ ...styles.inputStyle }} onChangeText={onChangeText} placeholder={placeholder} value={value} />
+         </View>
+      </View>
+   );
+};
+
+export default InputWithClearButton;
+
+const styles = StyleSheet.create({
+   root: {},
+   inputContainer: {
+      borderWidth: 0.5,
+      overflow: "hidden",
+      alignItems: "center",
+      borderRadius: appRadius.s,
+      flexDirection: "row-reverse",
+   },
+   inputStyle: {
+      flex: 1,
+      borderWidth: 0,
+   },
+   clearStyle: {
+      width: 30,
+      alignItems: "center",
+      justifyContent: "center",
+   },
+});
