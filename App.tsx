5154e42f47565a2ee1e881f94bde4bc57626006f
MrezaVasebi <reza.vaseb@gmail.com>
2023-10-07 16:13:10 +0330

    updated: package updated

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import { memo, useState } from "react";
+import { memo } from "react";
 import { StyleSheet } from "react-native";
 import { RootScreen } from "./components";
 import { appMargin } from "./utils";
 
 const App = () => {
-  const [showModal, setShowModal] = useState<boolean>(false);
-
   return <RootScreen rootStyle={styles.rootStyle}></RootScreen>;
 };
 
